052ba77dd6cd80cc68ad82d5bc9f273c59dc5b2c
JosephNaa <js.pekah@gmail.com>
2021-09-04 15:51:25 +0900

    apply swagger ui ignoring security

--- backend/api-server/src/main/java/com/slabel/global/config/security/config/WebSecurityConfig.java
+++ backend/api-server/src/main/java/com/slabel/global/config/security/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +35,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/webjars/**",
+                "/swagger-ui.html",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger/**");
     }
 
     @Bean
